cbf8ce7bf81ba6b5e1f8c9dfaf0920fc38448994
Juan Meta Sirgianto <juansirgianto.js@gmail.com>
2025-10-24 12:04:42 +0800

    rachael white

---
+++ src/app/casestudies/rachael-white/page.tsx
@@ -0,0 +1,9 @@
+import RachaelPage from "./rachael"
+
+export const metadata = {
+  title: 'LandscapeCC - Rachael White Designs',
+}
+
+export default function page() {
+  return <RachaelPage />
+}
--- src/app/casestudies/rachael-white/rachael.tsx
+++ src/app/casestudies/rachael-white/rachael.tsx
@@ -4,31 +4,30 @@ import Footer from '@/app/layouts/footer'
 import Navbar from '@/app/layouts/navbar'
 import React from 'react'
 
-function WlpPage() {
+function RachaelPage() {
   return (
     <div className='overflow-hidden'>
         <Navbar/>
         <div 
-        style={{ backgroundImage: "url('./we-love-plants/photos/1.jpg')" }}
+        style={{ backgroundImage: "url('./rachael-white/photos/1.jpg')" }}
         // style={{ backgroundImage: "url('./photos/1.jpg')" }}
          className='relative bg-cover bg-center md:h-[100vh] h-[50vh] mt-[73px] md:mt-0 lg:px-50 md:px-10 px-4' data-aos="fade-right">
           <div className="absolute inset-0 bg-black/70 z-0" />
           <div className='relative text-center text-white md:text-5xl text-3xl font-semibold top-1/2 -translate-y-1/2 md:px-0 px-2'>
             <h1 className='mb-5'>
-            We Love Plants
+            Rachael White Designs
             </h1>
           </div>
         </div>
         <div className='grid lg:grid-cols-2 md:gap-8 gap-4 items-center lg:px-30 md:px-10 px-4 lg:py-15 md:py-10 py-4'>
             <div data-aos="fade-right">
-                <h1 className='lg:text-xl md:text-lg text-sm lg:text-start text-center'>We Love Plants is a UK-based landscape and garden design studio delivering thoughtful, layered outdoor spaces across both residential and public realm projects. Known for their planting-led design approach, site responsiveness, and creative storytelling, their work ranges from immersive private gardens to large-scale civic interventions.</h1>
+                <h1 className='lg:text-xl md:text-lg text-sm lg:text-start text-center'>Rachael White Designs is a UK-based landscape design studio working on a range of projects including large country house estates, commercial landscapes, and rewilding-led schemes. Their work blends sensitivity to place with strategic, ecological planning.</h1>
             </div>
             <div className='flex flex-col justify-between h-full md:gap-5 gap-3'>
                 <h1 className='lg:text-3xl md:text-2xl text-lg font-semibold text-center' data-aos="fade-up">Services Applied :</h1>
                 <div className='grid grid-cols-2 gap-2'>
-                <h1 className='text-center lg:text-lg md:text-base text-xs rounded-full p-2 text-white bg-gradient-to-t from-[#757741] to-[#d3d2a5]' data-aos="fade-up">3D Garden Visualisation</h1>
-                <h1 className='text-center lg:text-lg md:text-base text-xs rounded-full p-2 text-white bg-gradient-to-t from-[#757741] to-[#d3d2a5]' data-aos="fade-up">Planting Atmosphere CGI</h1>
-                <h1 className='text-center lg:text-lg md:text-base text-xs rounded-full p-2 text-white bg-gradient-to-t from-[#757741] to-[#d3d2a5] col-span-2' data-aos="fade-up">Garden & Public Realm Animation</h1>
+                <h1 className='text-center lg:text-lg md:text-base text-xs rounded-full p-2 text-white bg-gradient-to-t from-[#757741] to-[#d3d2a5]' data-aos="fade-up">3D Visualisation</h1>
+                <h1 className='text-center lg:text-lg md:text-base text-xs rounded-full p-2 text-white bg-gradient-to-t from-[#757741] to-[#d3d2a5]' data-aos="fade-up">Animation</h1>
                 </div>
             </div>
         </div>
@@ -40,7 +39,7 @@ function WlpPage() {
           {/* Image 1 */}
           <div className="col-span-1 md:max-h-[60vh] overflow-hidden rounded-lg" data-aos="fade-right">
             <img 
-              src="./we-love-plants/photos/1.jpg"  
+              src="./rachael-white/photos/1.jpg"  
             //   src="./photos/1.jpg"  
               alt="Landscape design visualization 1" 
               className="w-full h-full object-cover"
@@ -49,7 +48,7 @@ function WlpPage() {
           {/* Image 2 */}
           <div className="col-span-2 md:max-h-[60vh] overflow-hidden rounded-lg" data-aos="fade-left">
             <img 
-              src="./we-love-plants/photos/2.jpg"
+              src="./rachael-white/photos/2.jpg"
             //   src="./photos/2.jpg"
               alt="Landscape design visualization 2" 
               className="w-full h-full object-cover "
@@ -59,17 +58,17 @@ function WlpPage() {
           {/* Image 3 */}
           <div className="col-span-3 md:max-h-[60vh] overflow-hidden rounded-lg" data-aos="fade-up">
             <img 
-              src="./we-love-plants/photos/3.jpg"
+              src="./rachael-white/photos/3.jpg"
             //   src="./photos/3.jpg"
               alt="Landscape design visualization 3" 
-              className="w-full h-full object-cover object-top "
+              className="w-full h-full object-cover "
             />
           </div>
 
          {/* Image 4 */}
           <div className="col-span-2 md:max-h-[60vh] overflow-hidden rounded-lg" data-aos="fade-right">
             <img 
-              src="./we-love-plants/photos/4.jpg"
+              src="./rachael-white/photos/4.jpg"
             //   src="./photos/4.jpg"
               alt="Landscape design visualization 3" 
               className="w-full h-full object-cover "
@@ -79,7 +78,7 @@ function WlpPage() {
          {/* Image 5 */}
           <div className="col-span-1 md:max-h-[60vh] overflow-hidden rounded-lg" data-aos="fade-left">
             <img 
-              src="./we-love-plants/photos/5.jpg"
+              src="./rachael-white/photos/5.jpg"
             //   src="./photos/5.jpg"
               alt="Landscape design visualization 3" 
               className="w-full h-full object-cover "
@@ -89,7 +88,7 @@ function WlpPage() {
          {/* Image 6 */}
           <div className="col-span-3 md:max-h-[60vh] overflow-hidden rounded-lg" data-aos="fade-up">
             <img 
-              src="./we-love-plants/photos/6.jpg"
+              src="./rachael-white/photos/6.jpg"
             //   src="./photos/6.jpg"
               alt="Landscape design visualization 3" 
               className="w-full h-full object-cover "
@@ -103,4 +102,4 @@ function WlpPage() {
   )
 }
 
-export default WlpPage
+export default RachaelPage
